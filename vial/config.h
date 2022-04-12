2a16e1a96475ce8f4bb7859cb19532afe5c54ca6
rooski15 <34818505+rooski15@users.noreply.github.com>
2022-04-12 08:43:42 -0700

    Add files via upload

---
+++ vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x6E, 0x7E, 0x95, 0x70, 0xF4, 0x06, 0x13, 0x1D}
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define VIAL_TAP_DANCE_ENTRIES 12
